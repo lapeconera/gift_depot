d36af03301efeb517ab74f07ac29e773efcacb74
adam22gary <48109783+adam22gary@users.noreply.github.com>
2019-03-06 11:28:21 +1100

    Delete security.rb

--- security.rb
+++
@@ -1,8 +0,0 @@
-xxxxxxxxxxxxxxxxxxxx
-# @security = {
-#             :q1 => "What is your favourite thing you like to eat?",
-#             :a1 => "bibimbap or pasta",
-#             :q2 => "Do you like red or white wine?",
-#             :a2 => "red",
-#             :robot => "Are you a robot?"
-#         }
